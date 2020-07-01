0ce5bbfe94c405af4548bacdc3d98c7578cd93e1
liushanshan <shanshan.liu@datacube.hk>
2020-07-02 06:08:22 +0800

    发布订阅新增取消订阅方法

--- observe.js
+++ observe.js
@@ -20,22 +20,40 @@ let dep={
         if (!fns||fns.length===0){
             return
         }
-        for(let i=0,;i<fns.length;i++){
+        for(let i=0;i<fns.length;i++){
             fns[i].apply(this, arguments)
         }
+    },
+    remove:function(type,fn){
+        let fns = this.subscribes[type]
+        if (!fns){ // 无人订阅
+            return
+        }
+        if (!fn){ //取消全部
+            fns && (fns.length=0)
+            return
+        }
+        let index=fns.indexOf(fn)
+        fns.splice(index,1)
+    }
+}
+
+let priceFn=function(name){
+    return function (price) {
+        console.log(name ,',您好售房价格更新:', price)
     }
 }
 
 // eg
-dep.subscribe('price',function(price){
-    console.log('lss,您好售房价格更新:',price)
-})
+dep.subscribe('price', priceFn('lss'))
 dep.subscribe('floor',function(floor){
     console.log('lss,您好，目前楼层',floor,'可用')
 })
 
-dep.subscribe('price',function(price){
-   console.log('张三,您好售房价格更新:', price)
-})
+dep.subscribe('price',priceFn('张三'))
 // 现在价格更新，需要发布通知
 dep.publish('price',5000)
+
+dep.remove('price',priceFn())
+
+dep.publish('price', 8000)
